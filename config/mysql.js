d6bd8601c5cfa6d272638e13bdb6b392202ec8b3
jainam240101 <jainamm47@gmail.com>
2024-02-10 01:19:20 -0500

    trying

--- config/mysql.js
+++ config/mysql.js
@@ -1,9 +1,16 @@
 module.exports = {
   development: {
+<<<<<<< HEAD
     username: "root",
     password: "root",
     database: "cloud",
     host: "localhost",
+=======
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: "mysql_cloud",
+>>>>>>> 0272ce9 (trying)
     dialect: "mysql",
   },
 };
