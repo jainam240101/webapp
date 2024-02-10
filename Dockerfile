0272ce981a5cf987a64956435ec5e3df4b30a26d
jainam240101 <jainamm47@gmail.com>
2024-02-10 01:19:20 -0500

    trying

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD ["sh", "-c", "sleep 20 && npm run dev"]
+CMD ["npm", "run" ,"dev"]
--- config/mysql.js
+++ config/mysql.js
@@ -3,7 +3,7 @@ module.exports = {
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
+    host: "mysql_cloud",
     dialect: "mysql",
   },
 };
