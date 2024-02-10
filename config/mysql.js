fcd23fdec2be23981f24ed36fc1dbbe1cfffa5ed
jainam240101 <jainamm47@gmail.com>
2024-02-10 11:18:28 -0500

    Added sleep to worklfow file

--- config/mysql.js
+++ config/mysql.js
@@ -3,7 +3,7 @@ module.exports = {
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    host: "mysql_cloud",
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   },
 };
