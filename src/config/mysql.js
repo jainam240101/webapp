759b17e3a7fd6436ac4756191bf0c1d736c7b1f7
jainam240101 <jainamm47@gmail.com>
2024-02-25 15:24:39 -0500

    Trying with root password

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    username: process.env.username,
+    username: process.env.username || "root",
     password: process.env.password,
     database: "webapp",
     host: process.env.host,
