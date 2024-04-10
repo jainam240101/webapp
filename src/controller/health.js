86ef2cbcb1027b52e072f811cdbf38113e09e601
jainam240101 <jainamm47@gmail.com>
2024-04-10 00:35:20 -0400

    Changing Healthz point

--- src/controller/health.js
+++ src/controller/health.js
@@ -9,7 +9,7 @@ const checkHealth = async (req, res) => {
     logger.info("MySQL Connection successful");
 
     res.setHeader("Cache-Control", "no-cache");
-    res.status(200).send();
+    res.status(200).send({ message: "All Good " });
   } catch (error) {
     logger.error("Error in Health Check ", error);
     res.setHeader("Cache-Control", "no-cache");
